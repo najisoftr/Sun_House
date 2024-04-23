eded48b84ab69f3c0650f458ba2ab1714c8c08a9
ahmed bouchelaghem <najisoftr@gmail.com>
2024-04-23 10:36:38 +0100

    frmMain 23-04-2024 10:36

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -28,22 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            AntdUI.MenuItem menuItem4 = new AntdUI.MenuItem();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.MenuLeft = new AntdUI.Menu();
+            this.MenuHorizontal = new AntdUI.Segmented();
+            this.PanelMain = new System.Windows.Forms.Panel();
+            this.tag1 = new AntdUI.Tag();
+            this.PanelMain.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MenuLeft
+            // 
+            this.MenuLeft.BackColor = System.Drawing.Color.Gold;
+            this.MenuLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            menuItem4.Icon = ((System.Drawing.Image)(resources.GetObject("menuItem4.Icon")));
+            menuItem4.Text = "My House";
+            this.MenuLeft.Items.AddRange(new AntdUI.MenuItem[] {
+            menuItem4});
+            this.MenuLeft.Location = new System.Drawing.Point(0, 136);
+            this.MenuLeft.Name = "MenuLeft";
+            this.MenuLeft.Size = new System.Drawing.Size(235, 612);
+            this.MenuLeft.TabIndex = 0;
+            this.MenuLeft.Text = "menu1";
+            // 
+            // MenuHorizontal
+            // 
+            this.MenuHorizontal.BackColor = System.Drawing.Color.Gold;
+            this.MenuHorizontal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MenuHorizontal.Location = new System.Drawing.Point(0, 0);
+            this.MenuHorizontal.Name = "MenuHorizontal";
+            this.MenuHorizontal.Size = new System.Drawing.Size(1082, 136);
+            this.MenuHorizontal.TabIndex = 1;
+            this.MenuHorizontal.Text = "segmented1";
+            // 
+            // PanelMain
+            // 
+            this.PanelMain.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PanelMain.BackgroundImage")));
+            this.PanelMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PanelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelMain.Controls.Add(this.tag1);
+            this.PanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelMain.Location = new System.Drawing.Point(235, 136);
+            this.PanelMain.Name = "PanelMain";
+            this.PanelMain.Size = new System.Drawing.Size(847, 612);
+            this.PanelMain.TabIndex = 2;
+            // 
+            // tag1
+            // 
+            this.tag1.Badge = "Community Edition";
+            this.tag1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tag1.Location = new System.Drawing.Point(0, 0);
+            this.tag1.Margin = new System.Windows.Forms.Padding(10);
+            this.tag1.Name = "tag1";
+            this.tag1.Padding = new System.Windows.Forms.Padding(10, 10, 10, 0);
+            this.tag1.Size = new System.Drawing.Size(845, 52);
+            this.tag1.TabIndex = 0;
+            this.tag1.Text = "Key Solutions Sun House";
+            this.tag1.Type = AntdUI.TTypeMini.Primary;
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(858, 658);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1082, 748);
+            this.Controls.Add(this.PanelMain);
+            this.Controls.Add(this.MenuLeft);
+            this.Controls.Add(this.MenuHorizontal);
             this.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FrmMain";
+            this.Text = "Key Solutions Sun House";
+            this.PanelMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private AntdUI.Menu MenuLeft;
+        private AntdUI.Segmented MenuHorizontal;
+        private System.Windows.Forms.Panel PanelMain;
+        private AntdUI.Tag tag1;
     }
 }
